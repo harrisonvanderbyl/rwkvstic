1066e153d9a53119ed76dc5d77588e992cb9387b
harrison <harrison.vanderbyl@gmail.com>
2023-02-24 21:07:43 +1100

    mm

--- src/rwkvstic/interOpLoaders/prequantized.py
+++ src/rwkvstic/interOpLoaders/prequantized.py
@@ -20,6 +20,8 @@ def loadPreQuantized(path, tokenizer=None):
         ww = ww[1].split(".")
         if int(ww[0]) > n_layers:
             n_layers = int(ww[0])
+        if weights[weight].dtype == torch.bfloat16:
+            weights[weight] = weights[weight].to(torch.float64)
 
     ops = RWKVCudaQuantOps(
         preQuantized=True, embed=len(weights["blocks.0.ln2.weight"]), layers=(n_layers+1), chunksize=32, target=100, maxQuantTarget=100, useLogFix="logfix" in path, useGPU=torch.cuda.is_available(), runtimedtype=torch.float64)
